b9fe10e888bb533361033e80092071817ef4ea0b
Dev. Pablo H. <devpabloh@gmail.com>
2024-07-30 17:39:27 -0300

    Update HookuseRef.js

--- 10_HOOKS/reacthooks/src/components/HookuseRef.js
+++ 10_HOOKS/reacthooks/src/components/HookuseRef.js
@@ -10,17 +10,25 @@ const HookuseRef = () => {
         numberRef.current = numberRef.current + 1;
     });
 
-
+    // 2 - useRef e DOM 
+    const inputRef = useRef()
+    const [text, setText] = useState("")
 
 
   return (
     <div>
         <h2>UseRef</h2>
+        {/* 1 - useRef */}
         <p>O componente renderizou: {numberRef.current} vezes</p>
         <p>Counter 1: {counter}</p>
         <button onClick={()=> setCounter(counter + 1)}>Contador A</button>
         <p>Counter 2: {counterB}</p>
         <button onClick={()=> setCounterB(counterB +1 )}>Contador B</button>
+        {/* 2 - useRef e DOM */}
+        <form>
+            <input type="text" ref={inputRef} />
+            <input type="submit" value="Enviar" />
+        </form>
         <hr />
     </div>
   )
