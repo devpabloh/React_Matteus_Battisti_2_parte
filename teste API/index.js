bc55efe3271520fe0f3017f7fdc89ba052a652ee
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-09 23:45:58 -0300

    Update index.js

--- teste API/index.js
+++ teste API/index.js
@@ -1,5 +1,7 @@
-const express = require("express")
-const app = express()
+const express = require("express") // aqui estamos carregando o módulo Express, que é uma biblioteca Node.js usada para construir aplicativos web e APIs de forma simplificada, e agora a variável express está armazenando o módulo Express que foi carregado. Agora podemos usar para configurar e iniciar um servidor web
+
+const app = express() // Aqui chamamos a função Express() o que cria uma nova instância de uma aplicação express, essa instância representada por app, será usada para definir rotas, middleware e outros aspectos do servidor
+
 
 app.use(
     express.urlencoded({
