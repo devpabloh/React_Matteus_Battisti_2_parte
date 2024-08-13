2d27e25dda35e9271504df50f7637a3901a2790f
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-12 21:21:49 -0300

    atualização

--- 3_API_STATUS_NA_RESPOSTA/index.js
+++ 3_API_STATUS_NA_RESPOSTA/index.js
@@ -17,6 +17,11 @@ app.post('/createproduct', (req, res)=>{
     const name = req.body.name
     const price = req.body.price
 
+    if(!name){
+        res.status(422).json({message: "O campo nome é obrigatório!"})
+        return
+    }
+
     console.log(name)
     console.log(price)
 
