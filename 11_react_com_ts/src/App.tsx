3895bf33820a79067d7f36b70194c2220b837306
Dev. Pablo H. <devpabloh@gmail.com>
2024-08-14 16:09:02 -0300

    atualização, criação de componentes, importação para o app.tsx

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// 4 - importação de componentes
+import FirstComponents from './components/FirstComponents';
+
 
 function App() {
+
+  // 1 - variaveis
+  const name: string = "Pablo";
+  const age: number = 30;
+  const isWorking: boolean = true;
+
+  // 2 - funções
+
+  const userGreeting = (name: string): string =>{
+    return ` Olá, ${name}`
+  }
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Age: {age}</p>
+      {isWorking && (
+        <div> 
+          <p>Está trabalhando...</p>  
+        </div>
+      )}
+      <h3>{userGreeting(name)}</h3>
+      <FirstComponents/>
     </div>
   );
 }
---
+++ 11_react_com_ts/src/components/FirstComponents.tsx
@@ -0,0 +1,11 @@
+import react, {ReactElement} from 'react'
+
+function FirstComponents(): ReactElement {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default FirstComponents
